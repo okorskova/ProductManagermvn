b11aa03c4aa2520b7b9d194de45c7e48d2762428
Olga Korskova <o-korskova@yandex.ru>
2022-08-31 20:12:26 +0300

    Added test in ProductManager

--- src/test/java/ru/netology/repository/ProductManagerTest.java
+++ src/test/java/ru/netology/repository/ProductManagerTest.java
@@ -14,17 +14,19 @@ public class ProductManagerTest {
 
     Product item1 = new Book(1, "Ёлка", "Елена Ким", 880);
     Product item2 = new Smartphone(2, "Xiaomi Poco M3", "Китай", 15990);
+    Product item3 = new Smartphone(3, "Xiaomi redmi note 11", "Китай", 19900);
 
     @BeforeEach
     public void setup() {
         manager.add(item1);
         manager.add(item2);
+        manager.add(item3);
     }
 
     @Test
     public void shouldShowAllItems() {
 
-        Product[] expected = {item1, item2};
+        Product[] expected = {item1, item2, item3};
         Product[] actual = manager.getItems();
 
         Assertions.assertArrayEquals(expected, actual);
@@ -56,5 +58,13 @@ public class ProductManagerTest {
 
         Assertions.assertArrayEquals(expected, actual);
 
+    }
+    @Test
+    public void shouldFindFewItems() {
+
+        Product[] expected = {item2, item3};
+        Product[] actual = manager.searchBy("Xiaomi");
+
+        Assertions.assertArrayEquals(expected, actual);
     }
 }
